becfedfda3ff126dc7f15ae49ca5d422788bd83a
Nikhil <niktemporary69@gmail.com>
2025-06-21 20:34:47 +0530

    use state

---
+++ 03_counter/src/App.jsx
@@ -0,0 +1,41 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+
+
+
+function App() {
+  const [counter, setCounter] = useState(15)
+
+function counterUp(){
+  if(counter == 20) return;
+  setCounter(counter + 1);
+  console.log(counter);
+}
+
+function counterDown(){
+  if(counter == 0) return;
+  setCounter(counter - 1);
+  console.log(counter);
+  
+}
+
+
+//To handle UI updation dynamically use hooks
+
+  return (
+    <>
+      <h1>React with ME</h1>
+      <h2>Counter: {counter}</h2>
+
+      <button onClick={counterUp}>Counter Up</button> <br /><br />
+      <button onClick={counterDown}>Count Down</button>
+
+      <p>Footer: {counter}</p>
+    </>
+  )
+}
+
+export default App
